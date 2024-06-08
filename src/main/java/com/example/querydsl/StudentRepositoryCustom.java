b285549557f92c1f8d954aaa00990c95edc36316
wjsdbsghks96@dgu.ac.kr <wjsdbsghks96@dgu.ac.kr>
2024-06-08 17:27:50 +0900

    feat: 동적 쿼리 사용을 위한 Boolean Expression 사용하기

--- src/main/java/com/example/querydsl/StudentRepositoryCustom.java
+++ src/main/java/com/example/querydsl/StudentRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.example.querydsl;
 
 import com.example.querydsl.entity.QStudent;
 import com.example.querydsl.entity.Student;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -33,5 +34,24 @@ public class StudentRepositoryCustom {
                 .from(QStudent.student)
                 .where(QStudent.student.name.eq(name))
                 .fetchFirst() != null;
+    }
+
+    public List<Student> findByNameAndAge(String name, int age) {
+        return jpaQueryFactory.selectFrom(QStudent.student)
+                .join(QStudent.student.academy).fetchJoin()
+                .where(searchConditionEquals(name, age))
+                .fetch();
+    }
+
+    private BooleanExpression searchConditionEquals(String name, int age) {
+        return NameEquals(name).and(AgeEquals(age));
+    }
+
+    private BooleanExpression NameEquals(String name) {
+        return name == null ? null : QStudent.student.name.eq(name);
+    }
+
+    private BooleanExpression AgeEquals(int age) {
+        return age == 0 ? null : QStudent.student.age.eq(age);
     }
 }
